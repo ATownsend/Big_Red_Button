815c3beb0ddedf91f1e721269afb42ef3e285161
Adrian <5705592+ATownsend@users.noreply.github.com>
2019-12-22 21:21:16 -0500

    spacing fix, remove tabs

--- big_red_button/__init__.py
+++ big_red_button/__init__.py
@@ -6,48 +6,48 @@ def threaded(fn):
     return wrapper
 
 class big_red_button:
-	def __init__(self, lightArray, button, sleepTime = 0.001):
-		import math
-		self.sleepTime = 0.001
-		self.button = button
-		#light animation pattern examples (4x4 lit and 4x4 blank)
-		#[[1,1],
-		# [1,1]],
-		#[[0,0],
-		# [0,0]],
-		self.lightArray = lightArray
-		self.lightAnimationPattern = [ [[  0,  0], [  1,  1]],    [[0.5,  0], [0.5,0.5]],    [[  1,0.5], [  1,  1]],    [[  1,  1], [0.5,0.5]],     [[0.5,  1], [  1,  1]],     [[  0,0.5], [0.5,0.5]] ]
-		self.lightPattern =[]
-	def setAnimationPattern(self, lightAnimationPattern):
-		self.lightAnimationPattern = lightAnimationPattern
-	def setToggleLights(self, lightPattern = self.currentLightPattern):
-		if self.toggle == True:
-			self.setLights([lightPattern[0], [0,0]])
-		elif self.toggle == False:
-			self.setLights([[0,0], lightPattern[1]])
-	def setLights(self, lightPattern = self.currentLightPattern):
-		for vertical in [0,1]:
-			for horizontal in [0,1]:
-				self.lightArray[vertical][horizontal].duty_cycle = self.lightValue(lightPattern[vertical][horizontal])
+    def __init__(self, lightArray, button, sleepTime = 0.001):
+        import math
+        self.sleepTime = 0.001
+        self.button = button
+        #light animation pattern examples (4x4 lit and 4x4 blank)
+        #[[1,1],
+        # [1,1]],
+        #[[0,0],
+        # [0,0]],
+        self.lightArray = lightArray
+        self.lightAnimationPattern = [ [[  0,  0], [  1,  1]],    [[0.5,  0], [0.5,0.5]],    [[  1,0.5], [  1,  1]],    [[  1,  1], [0.5,0.5]],     [[0.5,  1], [  1,  1]],     [[  0,0.5], [0.5,0.5]] ]
+        self.lightPattern =[]
+    def setAnimationPattern(self, lightAnimationPattern):
+        self.lightAnimationPattern = lightAnimationPattern
+    def setToggleLights(self, lightPattern = self.currentLightPattern):
+        if self.toggle == True:
+            self.setLights([lightPattern[0], [0,0]])
+        elif self.toggle == False:
+            self.setLights([[0,0], lightPattern[1]])
+    def setLights(self, lightPattern = self.currentLightPattern):
+        for vertical in [0,1]:
+            for horizontal in [0,1]:
+                self.lightArray[vertical][horizontal].duty_cycle = self.lightValue(lightPattern[vertical][horizontal])
 
-	@threaded
-	def runLights(self, lightAnimationPattern = self.lightAnimationPattern, sleepTime=self.sleepTime ):
-		while True:
-			currentState = self.toggle
-			for lightArray in lightAnimationPattern:
-				if currentState != self.toggle:
-					break
-	        	time.sleep(sleepTime)
-	        	self.setLights(lightArray)
-	def lightValue(self, brightness):
+    @threaded
+    def runLights(self, lightAnimationPattern = self.lightAnimationPattern, sleepTime=self.sleepTime ):
+        while True:
+            currentState = self.toggle
+            for lightArray in lightAnimationPattern:
+                if currentState != self.toggle:
+                    break
+                time.sleep(sleepTime)
+                self.setLights(lightArray)
+    def lightValue(self, brightness):
         lightValue = math.floor(65535*brightness)
         return lightValue
 
     @threaded
     def pressButton(self):
-    	self.button.wait_for_press()
-    	self.toggle = not self.toggle
+        self.button.wait_for_press()
+        self.toggle = not self.toggle
 
     @threaded
     def releaseButton(self):
-		self.button.wait_for_release()
+        self.button.wait_for_release()
