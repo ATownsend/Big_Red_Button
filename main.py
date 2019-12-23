50a7e7e30afd7c53dce4bdea66396c2af79f83d6
Adrian <5705592+ATownsend@users.noreply.github.com>
2019-12-22 23:36:46 -0500

    missing commas in hash table

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ pca.frequency = 60
 
 
 lightButtons = {
-    'leftHighButton'  : big_red_button([[pca.channels[12],pca.channels[13]],[pca.channels[14],pca.channels[15]]],Button(pin=12))
-    'leftRunButton'   : big_red_button([[pca.channels[8],pca.channels[9]],[pca.channels[10],pca.channels[11]]],Button(pin=16))
-    'rightHighButton' : big_red_button([[pca.channels[0],pca.channels[1]],[pca.channels[2],pca.channels[3]]],Button(pin=20))
+    'leftHighButton'  : big_red_button([[pca.channels[12],pca.channels[13]],[pca.channels[14],pca.channels[15]]],Button(pin=12)),
+    'leftRunButton'   : big_red_button([[pca.channels[8],pca.channels[9]],[pca.channels[10],pca.channels[11]]],Button(pin=16)),
+    'rightHighButton' : big_red_button([[pca.channels[0],pca.channels[1]],[pca.channels[2],pca.channels[3]]],Button(pin=20)),
     'rightRunButton'  : big_red_button([[pca.channels[0],pca.channels[1]],[pca.channels[2],pca.channels[3]]],Button(pin=21))
 }
 
