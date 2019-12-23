bda0bd134cbfcf41f56db5e1927d2b09dfa82d8b
Adrian <5705592+ATownsend@users.noreply.github.com>
2019-12-22 22:04:53 -0500

    fix line endings on all files

--- main.py
+++ main.py
@@ -18,4 +18,5 @@ pca.frequency = 60
 
 
 leftRunButton = big_red_button([pca.channels[8],pca.channels[9],pca.channels[10],pca.channels[11]],Button(pin=16))
+pca.channels[8].dutycycle = 0xffff
 leftRunButton.setLights([[1,1],[1,1]])
