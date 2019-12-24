7a81c57574f8cb7cfd7bfd10321dee41caeb1952
Adrian <5705592+ATownsend@users.noreply.github.com>
2019-12-24 08:05:20 -0500

    initial implementation of integration test

--- main.py
+++ main.py
@@ -49,12 +49,35 @@ def runButton(button, animation):
 def process_line(line, stdin, process):
     values = line.split(",")
     lightButtons[values[0]].setToggle(bool(values[1]))
-    print(values)
 
+def testLights(lights):
+    lightArray = lights.keys()
+    lightsFirst = [[0.5,0][0,0]]
+    lightsSecond = [[1,0.5][0,0]]
+    lightsThird = [[1,1][0,0]]
+
+    for light in lights:
+        lights[light].setLights(lightsFirst)
+        time.sleep(0.2)
+        lights[light].setLights(lightsSecond)
+        time.sleep(0.2)
+        lights[light].setLights(lightsThird)
+
+    lightsFirst = [[0,0.5][0,0]]
+    lightsSecond = [[0.5,1][0,0]]
+    lightsThird = [[1,1][0,0]]
+    for light in reversed(lightButtons):
+        lights[light].setLights(lightsFirst)
+        time.sleep(0.2)
+        lights[light].setLights(lightsSecond)
+        time.sleep(0.2)
+        lights[light].setLights(lightsThird)
+
+    
 
 def main():
-    for key in lightButtons:
-        #lightButtons[key].runLights()
+    testLights(lightButtons)
+for key in lightButtons:
         processKeeper[key] = sh.python3("main.py","--ButtonToRun", key ,"--AnimationPattern" ,json.dumps(lightButtons[key].getAnimationPattern()), _out=process_line, _bg=True)
     processKeeper['leftHighButton'].wait()
 
