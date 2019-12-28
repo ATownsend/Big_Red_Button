4767f3ca2ab823bfa355f806d64fd3441bc179ac
Adrian <5705592+ATownsend@users.noreply.github.com>
2019-12-27 19:52:30 -0500

    range broken for highlight animation pattern

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ highLightAnimationPattern = [ [[0.5,  1], [0.5,  1]],    [[  1,0.5], [0.5,  1]],
 
 #highLightAnimationPattern = [ [[0.2,0.2], [0.1,0.1]],  [[0.4,0.4], [0.2,0.2]],  [[0.6,0.6], [0.3,0.3]],  [[0.8,0.8], [0.4,0.4]],  [[1,1], [0.5,0.5]],  [[0.8,0.8], [0.6,0.6]],  [[0.6,0.6], [0.7,0.7]],   [[0.4,0.4], [0.8,0.8]],  [[0.2,0.2], [1,1]]  ]
 animationList = {}
-for counter in range(1..10):
+for counter in range(1,10):
     firstNumber = counter * 0.1
     secondNumber = counter * 0.2
     if secondNumber > 1: secondNumber = secondNumber - 1
